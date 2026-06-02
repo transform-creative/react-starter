7fd2d8b96cfae54c6bf1baec6b30f7eafd4b352f
Isaac Drury <idrury@transformcreative.com.au>
2026-06-02 13:37:19 +0930

    Remove bg references

--- app/presentation/authentication/Authentication.tsx
+++ app/presentation/authentication/Authentication.tsx
@@ -122,7 +122,7 @@ export function Authentication({}: AuthenticationProps) {
                 {processStarted == true ? (
                   <BeatLoader
                     size={10}
-                    color="var(--bg)"
+                    color="var(--bkg)"
                   />
                 ) : (
                   <div className="row middle center">
